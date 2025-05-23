0c1b84174eb348c3dae8a1a447426125ce105739
kjk <kjk@4science.net>
2025-05-23 13:29:09 +0900

    디비 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,3 +3,14 @@ from .base import *
 ALLOWED_HOSTS = ['13.125.137.210', 'bbangdorf.kro.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drfree',
+        'USER': 'drfree',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',   # or your RDS/EC2 IP
+        'PORT': '3306',
+    }
+}
